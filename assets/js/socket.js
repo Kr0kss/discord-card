a94035d948366813ccc5db4db880518263733db5
Kr0kss <65594227+Kr0kss@users.noreply.github.com>
2024-04-22 00:00:05 +0100

    Update socket.js

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -49,10 +49,7 @@ function fetchAndUpdateUserInfo(user_id) {
   fetch("https://api.krokss.com/get_userinfo/" + user_id)
     .then((response) => response.json())
     .then((data) => {
-      const { global_name, username } = data.data.user;
-      const { spotify } = data.data;
-      const { custom_status } = data.data.user_profile;
-      const status = data.status; // Ajuste para acessar o status corretamente
+      const { global_name, username, custom_status, status, spotify } = data;
       document.getElementById("at_sign").innerText = global_name;
       document.getElementById("nickname").innerText = username;
       document.getElementById("custom_status").innerText = custom_status;
